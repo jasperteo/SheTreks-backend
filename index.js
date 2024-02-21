ad1451d891d64a2124bbefcd240c75a1ebea32f6
jasperteo <jaspertzj@outlook.sg>
2024-02-21 21:59:39 +0800

    added hot reload upon file change, installed and setup simple express server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => res.json("Hello World!"));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
