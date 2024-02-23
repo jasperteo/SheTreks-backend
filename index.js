cd261283b56310096cfaacfc1a15c785d5737405
jasperteo <jaspertzj@outlook.sg>
2024-02-23 18:46:35 +0800

    trying hono instead of express

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-"use strict";
-
 import express from "express";
+import { Hono } from "hono";
 
-const app = express();
 const PORT = 8080;
+// const app = express();
 
-app.get("/", (req, res) => res.json("Hello World!"));
+// app.get("/", (req, res) => res.json("Hello World!"));
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+// app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+
+const app = new Hono();
+
+app.get("/", (c) => c.json("Hello World!"));
+
+export default { port: PORT, fetch: app.fetch };
