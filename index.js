c03762db9aa5480867bb0ef81a9f9ebf99f8658e
jasperteo <jaspertzj@outlook.sg>
2024-02-25 03:58:57 +0800

    initialize empty database with sequelize ORM

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-import express from "express";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-const PORT = 8080;
-// const app = express();
-
-// app.get("/", (req, res) => res.json("Hello World!"));
-
-// app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+const PORT = process.env.PORT;
 
 const app = new Hono();
 
+app.use(cors());
 app.get("/", (c) => c.json("Hello World!"));
 
 export default { port: PORT, fetch: app.fetch };
